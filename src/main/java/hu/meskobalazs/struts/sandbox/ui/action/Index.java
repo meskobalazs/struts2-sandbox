ff0616445fe121daf64c00df1b96d25d60813c28
meskobalazs <meskobalazs@gmail.com>
2016-05-12 13:44:25 +0200

    Struts2, Spring, JSP integrációs váz elkészült

---
+++ src/main/java/hu/meskobalazs/struts/sandbox/ui/action/Index.java
@@ -0,0 +1,14 @@
+package hu.meskobalazs.struts.sandbox.ui.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Index extends ActionSupport {
+
+	public static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() {
+		return SUCCESS;
+	}
+
+}
